09343bed3441ae70697125b316138085dd18a3ca
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-03-12 23:41:02 +0100

    remove obsolete files

--- Program.cs
+++
@@ -1,6 +0,0 @@
-using Player.Sharp;
-
-var builder = WebApplication.CreateBuilder(args);
-Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }).Build()
-    .Run();
