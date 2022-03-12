235373dadc676889cdd7f1505ab63cae9242054b
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-03-12 12:47:49 +0100

    add comment

--- src/Sharp.Player/Services/PlayerRegistrationService.cs
+++ src/Sharp.Player/Services/PlayerRegistrationService.cs
@@ -11,13 +11,14 @@ namespace Sharp.Player.Services;
 
 public class PlayerRegistrationService : IHostedService
 {
-    private readonly IPlayerRegistrationClient _playerRegistrationClient;
+    private readonly SharpDbContext _dbContext;
     private readonly PlayerDetailsOptions _detailsOptions;
     private readonly ILogger<PlayerRegistrationService> _logger;
-    private readonly SharpDbContext _dbContext;
+    private readonly IPlayerRegistrationClient _playerRegistrationClient;
 
     public PlayerRegistrationService(IPlayerRegistrationClient playerRegistrationClient,
-        IOptions<PlayerDetailsOptions> detailsOptions, ILogger<PlayerRegistrationService> logger, SharpDbContext dbContext)
+        IOptions<PlayerDetailsOptions> detailsOptions, ILogger<PlayerRegistrationService> logger,
+        SharpDbContext dbContext)
     {
         _playerRegistrationClient = playerRegistrationClient;
         _detailsOptions = detailsOptions.Value;
@@ -25,6 +26,11 @@ public class PlayerRegistrationService : IHostedService
         _dbContext = dbContext;
     }
 
+    // TODO: At the moment we're using a IHostedService for registering a player on the application startup. That might
+    //  not be the best idea as (in this implementation) it blocks the app and in addition the service registration
+    //  order must be preseerved. In general this is not a problem and it should work fine. Especially with the
+    //  constraint that a player MUST be registered and existent the blocking behaviour makes indeed the rest of the 
+    //  player safer. However, there must be a better way. 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         while (!cancellationToken.IsCancellationRequested)
@@ -38,7 +44,8 @@ public class PlayerRegistrationService : IHostedService
             var details = _dbContext.PlayerDetails.Find(name, email);
             if (details != null)
             {
-                _logger.LogWarning("Player details are already present. It could be invalidated by the game service. Hope for the best!");
+                _logger.LogWarning(
+                    "Player details are already present. It could be invalidated by the game service. Hope for the best!");
                 break;
             }
 
@@ -47,7 +54,7 @@ public class PlayerRegistrationService : IHostedService
             {
                 var response = await _playerRegistrationClient.GetPlayerDetails(name, email);
                 _logger.LogInformation("Successfully loaded player details");
-                _dbContext.PlayerDetails.Add(new(response.Name, response.Email, response.BearerToken));
+                _dbContext.PlayerDetails.Add(new PlayerDetails(response.Name, response.Email, response.BearerToken));
                 await _dbContext.SaveChangesAsync(cancellationToken);
                 break;
             }
@@ -58,9 +65,10 @@ public class PlayerRegistrationService : IHostedService
                     _logger.LogDebug("The player has not been registered yet.");
 
                     _logger.LogDebug("Register player");
-                    var response = await _playerRegistrationClient.CreatePlayer(new(name, email));
+                    var response = await _playerRegistrationClient.CreatePlayer(new PlayerRequest(name, email));
                     _logger.LogInformation("Successfully registered player");
-                    _dbContext.PlayerDetails.Add(new(response.Name, response.Email, response.BearerToken));
+                    _dbContext.PlayerDetails.Add(new PlayerDetails(response.Name, response.Email,
+                        response.BearerToken));
                     await _dbContext.SaveChangesAsync(cancellationToken);
                     break;
                 }
