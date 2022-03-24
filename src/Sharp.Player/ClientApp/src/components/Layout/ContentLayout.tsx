220aa5dc12515d070085534b411b8dd820bc114a
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-03-24 14:20:23 +0100

    Add a custom navbar to content layout

--- src/Sharp.Player/ClientApp/src/components/Layout/ContentLayout.tsx
+++ src/Sharp.Player/ClientApp/src/components/Layout/ContentLayout.tsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
 
 import { Head } from '../Head';
+import {Navbar} from "../Navbar";
+
+type TitleProps = {
+    title?: string;
+}
+
+const Title = ({title}: TitleProps) => {
+    if(title) {
+        return (
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
+            </div>
+        );
+    }
+    return (null);
+}
 
 type ContentLayoutProps = {
-  children: React.ReactNode;
-  title: string;
+    children: React.ReactNode;
+    title?: string;
 };
 
 export const ContentLayout = ({ children, title }: ContentLayoutProps) => {
   return (
     <>
       <Head title={title} />
+      <Navbar />
       <div className="py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
-        </div>
+          <Title title={title} />
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">{children}</div>
       </div>
     </>
---
+++ src/Sharp.Player/ClientApp/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import {FolderIcon, HomeIcon} from "@heroicons/react/outline";
+import {NavLink} from "react-router-dom";
+import clsx from "clsx";
+
+type NavigationItem = {
+    name: string;
+    to: string;
+};
+
+const NavigationButtons = () => {
+    const navigation = [
+        { name: 'Home', to: '.' },
+        { name: 'Map', to: './map' }
+    ] as NavigationItem[];
+
+    return (
+        <>
+            {navigation.map((item, index) => (
+                <NavLink
+                    end={index === 0}
+                    key={item.name}
+                    to={item.to}
+                    className={({isActive}) => clsx('px-3 py-2 rounded-md text-sm font-medium', isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")}
+                >
+                    {item.name}
+                </NavLink>
+            ))}
+        </>
+    );
+};
+
+export const Navbar = () => {
+    return (
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+                    <div className="flex-1 flex items-stretch justify-start">
+                        <div className="block ml-6 flex space-x-4">
+                            <NavigationButtons />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
